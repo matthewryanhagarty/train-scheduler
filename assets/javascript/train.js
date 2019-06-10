d192908ebe190c191e110e0db4c65d79628f38f8
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-06-09 19:28:49 -0700

    keeping project up to date before working on it with classmates.

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,36 +1,63 @@
-
-
-  var firebaseConfig = {
-    apiKey: "AIzaSyCFm0KFQ-GDKkPEZMByAqHJoTG4WN5TPR0",
-    authDomain: "casual-salad-redo.firebaseapp.com",
-    databaseURL: "https://casual-salad-redo.firebaseio.com",
-    projectId: "casual-salad-redo",
-    storageBucket: "casual-salad-redo.appspot.com",
-    messagingSenderId: "730940020222",
-    appId: "1:730940020222:web:139c207263441203"
+var firebaseConfig = {
+    apiKey: "AIzaSyBltnyVVgAHFPDSvrjvcFHpTpMg4PZghZc",
+    authDomain: "matthewryanhag.firebaseapp.com",
+    databaseURL: "https://matthewryanhag.firebaseio.com",
+    projectId: "matthewryanhag",
+    storageBucket: "matthewryanhag.appspot.com",
+    messagingSenderId: "473975414003",
+    appId: "1:473975414003:web:230ab98b5acb5d9a"
   };
 
   firebase.initializeApp(firebaseConfig);
 
-  var dataRef = firebase.database();
+
+  var database = firebase.database();
 
   var trainName = "";
   var destination= "";
   var firstTrainTime = "";
   var frequency = "";
 
-  $("#submit-train").on("click", function(event) {
+  $(".submit").on("click", function(event) {
     event.preventDefault();
 
-  tainName = $("#train-name-input").val().trim();
-  destination =$("#destination-input").val().trim();
-  firstTrainTime = $("#first-train-input").val().trim();
-  frequency = $("#frequency-input").value().trim();
+  tainName = $("#train-name-input").val().trim()
+  destination =$("#destination-input").val().trim()
+  firstTrainTime = $("#first-train-input").val().trim()
+  frequency = $("#frequency-input").value().trim()
 
-  dataRef.ref().push({
-      trainNameF: trainName,
-      destinationF: destination,
-      firstTrainTimeF: firstTrainTime,
-      frequencyF: frequency,
+  database.ref().push({
+      trainName: trainName,
+      destination: destination,
+      firstTrainTime: firstTrainTime,
+      frequency: frequency,
   });
 });
+
+    database.ref("train-scheduler").on("child_added", function(snapshot) {
+
+        var trainScheduler = snapshot.val();
+
+        console.log(trainScheduler.trainName);
+
+        var tableRow = $("<tr>");
+
+        tableRow.append($("<td>").text(trainScheduler.trainName));
+        tableRow.append($("<td>").text(trainScheduler.destination));
+        tableRow.append($("<td>").text(trainScheduler.firstTrainTime));
+        tableRow.append($("<td>").text(trainScheduler.frequency));
+
+        var tableBody = $("<tbody>");
+
+        tableBody.append(tableRow);
+    },
+        function(errorObject) {
+            console.log("Errors handled: " + errorObject.code)
+        })
+
+        var tableRow = $("<tr>");
+
+        tableRow.append("<td>" + trainName + "</td>")
+        tableRow.append("<td>" + trainName + "</td>")
+        tableRow.append("<td>" + trainName + "</td>")
+        tableRow.append("<td>" + trainName + "</td>")
